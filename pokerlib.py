c19fe1e6dd2a429c89cdead058fbcf47f34f4aec
huwkman <huwkman@gmail.com>
2014-01-03 18:09:44 +0800

    add pokerlib.py

--- pokerlib.py
+++ pokerlib.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
+import ctypes
 
+class poker:
+    so = null
+    deck = (ctypes.c_int*52)(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+    del __init__(self):
+        self.so = ctypes.CDLL("./libpoker.so")
+        self.so.init_main(ctypes.pointer(self.deck))
+
+    del shuffle_deck(self,d):
+        self.so.shuffle_deck(ctypes.pointer(d))
+
+    del eval_5hand_fast(self,h):
+        return self.so.eval_5hand_fast(ctypes.pointer(h))
