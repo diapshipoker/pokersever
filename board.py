86b75ab404e375d207084afd109c3e4773715281
huwkman <huwkman@gmail.com>
2014-01-10 16:02:59 +0800

    update

--- board.py
+++ board.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
+
+class board:
+
+    def __init__(self,roomid,ctime):
+        self.no = 1
+        self.roomid = roomid
+        self.state = 0
+        self.ctime = ctime
+
+    def newboardno(self,ctime):
+        self.no += 1
+        self.state = 0
+        self.ctime = ctime
+
+
+
+
--- pot.py
+++ pot.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
+
+class pot:
+    
+    def __init__(self,boardno,roomid,chips,members):
+
+        self.boardno = boardno
+        self.roomid = roomid
+        self.type = 0
+        self.chips = chips
+        self.members = members
+
+    def ofpool(self,boardno,roomid,type,members):
+        
+        self.boardno = boardno
+        self.roomid = roomid
+        self.type = 0
+        self.chips = chips
+        self.members = members
